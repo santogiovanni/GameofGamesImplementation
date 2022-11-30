579d1fcc6a71a0b086460b4e38b5c2683d836f79
Kiet Ha <culpritix@gmail.com>
2022-11-30 16:13:42 -0500

    Adding global score to coinflip

--- CoinFlip.java
+++ CoinFlip.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class CoinFlip {
+public class CoinFlip extends PlayGames {
 
     //Method called in main is used to run entire game
     public static void flip(){
@@ -13,7 +13,7 @@ public class CoinFlip {
         Scanner obj = new Scanner(System.in);
         int numRounds;
 
-        //user is asked how many rounds they want to play and their input is saved  
+        //user is asked how many rounds they want to play and their input is saved
         System.out.println("How many rounds do you want to play ? You will play to the best of that number of rounds. Number of rounds must be odd.\n");
         numRounds = obj.nextInt();
 
@@ -23,7 +23,7 @@ public class CoinFlip {
 
             numRounds = obj.nextInt();
         }
-        
+
         //target of wins created for number of
         //if win total exceeds this number, player or computer wins
         int target = numRounds/2;
@@ -32,14 +32,14 @@ public class CoinFlip {
         for(int i = 1; i<=numRounds; i++){
             //round number is displayed
             System.out.println("Round: " + i + "\n");
-            
+
             //user is prompted to pick 1 for heads or 0 for tails
             //input is read and saved in int choice
             int choice;
             Scanner obj2 = new Scanner(System.in);
             System.out.println("User1, insert 1 if you choose heads or 0 if tails");
             choice = obj2.nextInt();
-    
+
             //while loop occurs if choice is not either 0 or 1
             while(choice != 1 && choice != 0){
                 System.out.println("Insert Valid Number\n");
@@ -58,7 +58,7 @@ public class CoinFlip {
             //random number is generated based on coin flip, number is either 0 or 1
             Random rand = new Random(); //instance of random class
             int upperbound = 2;
-            int randNum = rand.nextInt(upperbound); 
+            int randNum = rand.nextInt(upperbound);
 
             //if random number is 0 tails is correct answer, if number is 1 heads is correct answer
             //if statement checks which random number is chosen
@@ -97,10 +97,12 @@ public class CoinFlip {
             //if one of these conditions is true, winner is printed, then the for loop breaks ending the game
             if(userPts>target){
                 System.out.println("You Win Coin Toss Game\n");
+                globalUserPts++;
                 break;
             }
             if(compPts>target){
                 System.out.println("You Lose Coin Toss Game\n");
+                globalComputerPts++;
                 break;
             }
 
@@ -109,10 +111,10 @@ public class CoinFlip {
     }
 
     //main used to call the game in flip()
-    public static void main(String args[]) {
+    public static void main() {
         //print statement used to explain the rules of the game
-        System.out.println("User1 will choose with heads and tails. The computer will generate a random choice. If User1’s choice is correct, User1 receives a point, otherwise User2 receives a point. Follow the instructions that will be displayed as you play. Good Luck!. If playing against computer, use 'computer' instead of 'User2'\n");
+        System.out.println("\nUser1 will choose with heads and tails. The computer will generate a random choice. If User1’s choice is correct, User1 receives a point, otherwise User2 receives a point. Follow the instructions that will be displayed as you play. Good Luck!. If playing against computer, use 'computer' instead of 'User2'\n");
         flip();
-        
+
     }
 }
