73b75293e61a70cab6a7ba7fe20f20bcd2f1b6be
Kiet Ha <culpritix@gmail.com>
2022-11-30 17:18:47 -0500

    formatting issues + adding global score

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;   //import scanner
 import java.util.Random;    //import random
 
-public class GuessTheNumber{
+public class GuessTheNumber extends PlayGames {
 
-    public static void getInput(){
+    public static void getInput() {
         Scanner sc = new Scanner(System.in);    //create our scanner object
         //int variables in the program
         int range = 0;
@@ -15,11 +15,11 @@ public class GuessTheNumber{
         boolean rFlag = true;
         boolean tFlag = true;
         boolean oFlag = true;
-        
+
 
 
         while(rFlag){
-            System.out.println("Pick the max number of a range starting at 1");   //prompt the user for input
+            System.out.println("\nPick the max number of a range starting at 1");   //prompt the user for input
             range = sc.nextInt();   //get user input for range
 
             if(range <= 1){     //make sure the user entered a valid input
@@ -28,7 +28,7 @@ public class GuessTheNumber{
                 rFlag = false;      //if the user gave valid input, set our flag to false so we fall out of the loop
             }
         }
-        
+
         while(tFlag){
             System.out.println("Pick a number that the other player will try to guess.");   //prompt for user input
             numToGuess = sc.nextInt();    //get user input for the number the computer will try to guess
@@ -41,7 +41,7 @@ public class GuessTheNumber{
         }
 
         limit = range / 2;      //make a limit to make sure the number of guesses input is valid
-        
+
         while(oFlag){
             System.out.println("Pick a number of attempts the other player will have to guess your number. Remember, it cannot be more than half the number of values in range.");    //prompt for user input
             numOfGuesses = sc.nextInt();    //get user input for the number of guesses the other person will have
@@ -60,9 +60,10 @@ public class GuessTheNumber{
         System.out.println("The range is 1 to "+ range +". You have got "+ numOfGuesses +" attempts. Insert a guess within the range.");
 
         while(gFlag){
-            System.out.println("Player 2 guessed " + randNum);
+            System.out.println("\nPlayer 2 guessed " + randNum);
             if(randNum == numToGuess){
                 System.out.println("Player 2 wins!");
+                globalComputerPts++;
                 gFlag = false;
             } else if(numOfGuesses != 0){
                 System.out.println("Wrong guess! You still have attempts left. Guess again!");
@@ -72,21 +73,19 @@ public class GuessTheNumber{
             numOfGuesses--;
 
             if(numOfGuesses == 0){
-                System.out.println("The correct guess is "+ numToGuess +". Player 1 Wins");
+                System.out.println("\nThe correct guess is "+ numToGuess +". Player 1 Wins");
+                globalUserPts++;
                 gFlag = false;
             }
         }
-        
 
-    
     }
-    
 
 
-    public static void main(String[] args){
-        
 
-        System.out.println("Player 1 will pick a number secretly and store it somewhere. Player 1 will then specify the allowed number of guessing attempts. Attention, the number of guesses cannot be more than half the number of values in range. Player 2 will keep guessing the number until their attempts are over. If Player 2 guesses the number in those attempts, they win. If they don't guess the number within the allowed attempts, they lose and player 1 wins. Good luck!");
+    public static void main(String[] args) {
+
+        System.out.println("\nPlayer 1 will pick a number secretly and store it somewhere. Player 1 will then specify the allowed number of guessing attempts. Attention, the number of guesses cannot be more than half the number of values in range. Player 2 will keep guessing the number until their attempts are over. If Player 2 guesses the number in those attempts, they win. If they don't guess the number within the allowed attempts, they lose and player 1 wins. Good luck!");
 
         getInput();     //call the getInput function to get all of the input from our user
 
