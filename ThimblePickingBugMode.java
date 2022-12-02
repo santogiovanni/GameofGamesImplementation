6667fd192d51bc04331651ab902a69e0d536e9c2
Stephen Antogiovanni <106194360+santogiovanni@users.noreply.github.com>
2022-12-02 15:16:47 -0500

    Update ThimblePickingBugMode.java

--- ThimblePickingBugMode.java
+++ ThimblePickingBugMode.java
@@ -121,9 +121,6 @@ public class ThimblePicking extends PlayGames {
                 globalComputerPts++;
                 break;
             }
-
-          
-            // S14: End “Thimble Picking” game. And go back to the master branch @S04
         }
     }
 }
